2b2ac5e707cdba4837634f109ba854e234fd0510
Donnuel Fernandez <92870072+poltato-dev@users.noreply.github.com>
2022-03-31 01:55:52 +0800

    Add project files.

---
+++ CapstoneProject_PayrollSystem_v1/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(CapstoneProject_PayrollSystem_v1.Startup))]
+namespace CapstoneProject_PayrollSystem_v1
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
